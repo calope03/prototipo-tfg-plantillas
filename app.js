b68f398e776465c9f132c5dd70dc1c126ad7ddbe
Carmen Lopez <carmenlop94@gmail.com>
2018-12-19 15:18:44 +0000

    add rectangulo resizable

--- app.js
+++ app.js
@@ -84,12 +84,13 @@ interact('.dropzone').dropzone({
   },
   ondrop: function (event) {
     console.log(event);
-    if(event.relatedTarget.classList.contains('draggable')){
+    if(event.relatedTarget.classList.contains('draggable') && event.relatedTarget.classList.contains('pictograma')){
       creaPicto(event);
       noMover(event.relatedTarget);
+    }else if(event.relatedTarget.classList.contains('draggable') && event.relatedTarget.classList.contains('newArea')){
+      creaArea(event);
+      noMover(event.relatedTarget);
     }
-    
-    
   },
   ondropdeactivate: function (event) {
     // remove active dropzone feedback
@@ -111,40 +112,59 @@ interact('.draggado')
     // call this function on every dragmove event
     onmove: dragMoveListener,
     // call this function on every dragend event
-    onend: function (event) {
-      var textEl = event.target.querySelector('p');
-    }
+    onend: function (event) {}
 });
 
 
+interact('.rectdraggable')
+  .draggable({
+    onmove: window.dragMoveListener,
+    restrict: {
+      restriction: 'parent',
+      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    }
+  })
+  .resizable({
+    // resize from all edges and corners
+    edges: { left: true, right: true, bottom: true, top: true },
+    // keep the edges inside the parent
+    restrict: {
+      restriction: "parent",
+      endOnly: true,
+    },
+    restrictEdges: {
+      outer: 'parent',
+      endOnly: true,
+    },
 
-function creaPicto(event){
-  var pictograma = event.relatedTarget;
-  var posX = event.interaction.curCoords.client.x;
-  var posY = event.interaction.curCoords.client.y;
-  var dropZone = event.target;
-  
-  console.log("posx:",posX,"posy:",posY);
-  var pictoclonado = pictograma.cloneNode(true); 
-  pictoclonado.classList.remove('col-md-4');
-  pictoclonado.classList.remove('draggable');
-  pictoclonado.classList.add('draggado');
-  var anchoPicto = pictograma.clientWidth;
-  var altoPicto = pictograma.clientHeight;
-  pictoclonado.style.width = anchoPicto+"px";
-  pictoclonado.style.height = altoPicto+"px";
-  var posicionZone = dropZone.getBoundingClientRect();
-  
-  pictoclonado.style.left=(posX - (anchoPicto/2)-posicionZone.left)+"px";
-  pictoclonado.style.top=(posY - (altoPicto/2)-posicionZone.top)+"px";
-  
-  var despVer = 566*100/myHeight;
-  pictoclonado.style.position="absolute";
-  console.log("posfortop:",pictoclonado.style.top,"posforleft:",pictoclonado.style.left);
-  noMover(pictoclonado);
-  var divGrande =document.getElementById("dropzone");
-  divGrande.appendChild(pictoclonado);
-}
+    // minimum size
+    restrictSize: {
+      min: { width: 100, height: 50 },
+    },
+
+    inertia: true,
+  })
+  .on('resizemove', function (event) {
+    var target = event.target,
+        x = (parseFloat(target.getAttribute('data-x')) || 0),
+        y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+    // update the element's style
+    target.style.width  = event.rect.width + 'px';
+    target.style.height = event.rect.height + 'px';
+
+    // translate when resizing from top or left edges
+    x += event.deltaRect.left;
+    y += event.deltaRect.top;
+
+    target.style.webkitTransform = target.style.transform =
+        'translate(' + x + 'px,' + y + 'px)';
+
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+    
+  });
+
 
 function noMover(elemento){
   elemento.style.webkitTransform =
@@ -156,6 +176,46 @@ function noMover(elemento){
   elemento.setAttribute('data-y', 0);
 }
 
+function creaPicto(event){
+  var pictoclonado = colocaElemento(event);
+  pictoclonado.classList.remove('col-md-4');
+  pictoclonado.classList.remove('draggable');
+  pictoclonado.classList.add('draggado');
+}
+
+function creaArea(event){
+  var areaClonada = colocaElemento(event);
+  areaClonada.classList.remove('col-md-6');
+  areaClonada.classList.remove('draggable');
+  areaClonada.classList.add('rectdraggable');
+}
+function colocaElemento(event){
+  var elemento = event.relatedTarget;
+  var posX = event.interaction.curCoords.client.x;
+  var posY = event.interaction.curCoords.client.y;
+  var dropZone = event.target;
+  
+  console.log("posx:",posX,"posy:",posY);
+  var elementoClonado = elemento.cloneNode(true); 
+  
+  var anchoPicto = elemento.clientWidth;
+  var altoPicto = elemento.clientHeight;
+  elementoClonado.style.width = anchoPicto+"px";
+  elementoClonado.style.height = altoPicto+"px";
+  var posicionZone = dropZone.getBoundingClientRect();
+  
+  elementoClonado.style.left=(posX - (anchoPicto/2)-posicionZone.left)+"px";
+  elementoClonado.style.top=(posY - (altoPicto/2)-posicionZone.top)+"px";
+  
+  var despVer = 566*100/myHeight;
+  elementoClonado.style.position="absolute";
+  console.log("posfortop:",elementoClonado.style.top,"posforleft:",elementoClonado.style.left);
+  noMover(elementoClonado);
+  var divGrande =document.getElementById("dropzone");
+  divGrande.appendChild(elementoClonado);
+  
+  return elementoClonado;
+}
 
 
 ////http://mathelino.com/index.php?id=64
