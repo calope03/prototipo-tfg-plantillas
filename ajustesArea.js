86f189ef2955d0a7e87db6e9a820a9e6935ef57a
calope03 <calope03@ucm.es>
2019-04-04 23:46:46 +0200

    add listas privadas con miniatura y botones nuevos

--- ajustesArea.js
+++ ajustesArea.js
@@ -1,29 +1,28 @@
-var ajustesArea = function (area) {
-    
-    var areaPadre = area;
-    /*var pictoImg = pictogramaPadre.children[0];
+var ajustesArea = function(area) {
+  var areaPadre = area;
+  /*var pictoImg = pictogramaPadre.children[0];
     var pictoText = pictogramaPadre.children[1];
     var pictoHover = pictogramaPadre.children[2];*/
-    var areaText = areaPadre.children[0];
-    var panelAjustes = document.getElementById('panelDerechaAjustesArea');
-    var colorBorde = document.getElementById("color-borde-area");
-    var colorFondo = document.getElementById("color-fondo-area");
-    var nombreAreaField = document.getElementById('nombreArea');
-    var visibleONo = document.getElementById('customCheck3');
-    //var pictoOriginal = pictogramaPadre.cloneNode(true);
-    var panelDerecho = document.getElementById('panelDerecha');
-    //var seleccionHover = document.getElementById('seleccionHover');
-    var botonCerrar = document.getElementById('botonCerrarArea');
-    var _self = this;
-    //console.log(pictoHijos);
-    /*
+  var areaText = areaPadre.children[0];
+  var panelAjustes = document.getElementById("panelDerechaAjustesArea");
+  var colorBorde = document.getElementById("color-borde-area");
+  var colorFondo = document.getElementById("color-fondo-area");
+  var nombreAreaField = document.getElementById("nombreArea");
+  var visibleONo = document.getElementById("customCheck3");
+  //var pictoOriginal = pictogramaPadre.cloneNode(true);
+  var panelDerecho = document.getElementById("panelDerecha");
+  //var seleccionHover = document.getElementById('seleccionHover');
+  var botonCerrar = document.getElementById("botonCerrarArea");
+  var _self = this;
+  //console.log(pictoHijos);
+  /*
     var dropzone = document.getElementById("dropzone");
     var panelDerecho = document.getElementById('panelDerecha');
     var dropZoneOriginal = dropzone.cloneNode(false);
     var panelAjustes = document.getElementById('panelDerechaAjustes');
     */
-    
-   /* function aceptarOCancelar(evento){
+
+  /* function aceptarOCancelar(evento){
       //console.log(evento);
       if(evento.isTrusted){
         var clases = evento.target.classList;
@@ -41,33 +40,33 @@ var ajustesArea = function (area) {
         
       }
     }*/
-    
-    function cambiaTitulo(evento){
-        console.log(evento);
-        areaText.innerText = evento.target.value;
+
+  function cambiaTitulo(evento) {
+    console.log(evento);
+    areaText.innerText = evento.target.value;
+  }
+
+  function cambiaColorBorde(evento) {
+    if (evento.isTrusted) {
+      areaPadre.style.borderColor = evento.target.value;
     }
-    
-    function cambiaColorBorde(evento){
-      if(evento.isTrusted){
-          areaPadre.style.borderColor = evento.target.value;
-      }
+  }
+
+  function cambiaColorFondo(evento) {
+    if (evento.isTrusted) {
+      areaPadre.style.backgroundColor = evento.target.value;
     }
-    
-    function cambiaColorFondo(evento){
-      if(evento.isTrusted){
-          areaPadre.style.backgroundColor = evento.target.value;
-      }
+  }
+
+  function mostrarTitulo(event) {
+    if (event.target.checked) {
+      areaText.style.display = "block";
+    } else {
+      areaText.style.display = "none";
     }
-    
-    function mostrarTitulo(event){
-      if (event.target.checked) {
-        areaText.style.display = 'block';
-      } else {
-        areaText.style.display = 'none';
-      }
-      console.log("hooooola");
-    };
-    /*
+    console.log("hooooola");
+  }
+  /*
     function cambiaHover(event){
       console.log('salud');
       if(event.target.classList.contains('fa-times')){
@@ -86,39 +85,39 @@ var ajustesArea = function (area) {
         console.log('salud');
       }
     }*/
-    
-    function cerrar(){
-      panelAjustes.style.display = 'none';
-      panelDerecho.style.display = 'block';
-      _self.stop();
-    }
 
-    this.init = function(){
-      botonCerrar.addEventListener('click', cerrar);
-      //seleccionHover.addEventListener('click',cambiaHover);
-      nombreAreaField.value = areaText.innerText;
-      colorBorde.addEventListener('change',cambiaColorBorde);
-      colorFondo.addEventListener('change',cambiaColorFondo);
-      nombreAreaField.addEventListener('keyup',cambiaTitulo);
-      visibleONo.addEventListener('change', mostrarTitulo);
-      panelAjustes.style.display = 'block';
-      areaPadre.classList.add('myShadow');
+  function cerrar() {
+    panelAjustes.style.display = "none";
+    panelDerecho.style.display = "block";
+    _self.stop();
+  }
+
+  this.init = function() {
+    botonCerrar.addEventListener("click", cerrar);
+    //seleccionHover.addEventListener('click',cambiaHover);
+    nombreAreaField.value = areaText.innerText;
+    colorBorde.addEventListener("change", cambiaColorBorde);
+    colorFondo.addEventListener("change", cambiaColorFondo);
+    nombreAreaField.addEventListener("keyup", cambiaTitulo);
+    visibleONo.addEventListener("change", mostrarTitulo);
+    panelAjustes.style.display = "block";
+    areaPadre.classList.add("myShadow");
     //  console.log(visibleONo);
-    }
-    
-    this.stop = function(){
-      botonCerrar.removeEventListener('click', cerrar);
-      //panelAjustes.removeEventListener('click', aceptarOCancelar);
-      //seleccionHover.removeEventListener('click',cambiaHover);
-     // nombrePictoField.value = pictoText.innerText;
-      colorBorde.removeEventListener('change',cambiaColorBorde);
-      colorFondo.removeEventListener('change',cambiaColorFondo);
-      nombreAreaField.removeEventListener('keyup',cambiaTitulo);
-      visibleONo.removeEventListener('change', mostrarTitulo);
-      //panelAjustes.style.display = 'block';
-      areaPadre.classList.remove('myShadow');
-      
-      /*if(cancelar){
+  };
+
+  this.stop = function() {
+    botonCerrar.removeEventListener("click", cerrar);
+    //panelAjustes.removeEventListener('click', aceptarOCancelar);
+    //seleccionHover.removeEventListener('click',cambiaHover);
+    // nombrePictoField.value = pictoText.innerText;
+    colorBorde.removeEventListener("change", cambiaColorBorde);
+    colorFondo.removeEventListener("change", cambiaColorFondo);
+    nombreAreaField.removeEventListener("keyup", cambiaTitulo);
+    visibleONo.removeEventListener("change", mostrarTitulo);
+    //panelAjustes.style.display = 'block';
+    areaPadre.classList.remove("myShadow");
+
+    /*if(cancelar){
         console.log(pictogramaPadre);
         let padre =pictogramaPadre.parentNode;
         if(padre){
@@ -126,7 +125,6 @@ var ajustesArea = function (area) {
         }
         
       }*/
-      console.log('parando picto ');
-    }
-  
+    console.log("parando picto ");
+  };
 };
--- guarda.js
+++ guarda.js
@@ -1,55 +1,56 @@
-function navigate (path) {
+function navigate(path) {
   var current = window.location.href;
-  window.location.href = current.replace(/#(.*)$/, '') + '#' + path;
+  window.location.href = current.replace(/#(.*)$/, "") + "#" + path;
 }
 
-
-document.getElementById("botonDescarga").addEventListener('click', function(event){
-   let dropzone = document.getElementById("dropzone");
-   let dropzoneOuter = document.getElementById("dropzone").outerHTML;
-   let hash = hashActual();
-   let tipo;
-   let id = guid();
-   let tamanoDropzone = {
-       alto: dropzone.clientHeight,
-       ancho: dropzone.clientWidth
-   }
-   console.log()
-   var miniatura ="";
-   html2canvas(dropzone, {
-      onrendered (canvas) {
+document
+  .getElementById("botonDescarga")
+  .addEventListener("click", function(event) {
+    let dropzone = document.getElementById("dropzone");
+    let dropzoneOuter = document.getElementById("dropzone").outerHTML;
+    let hash = hashActual();
+    let tipo;
+    let id = guid();
+    let tamanoDropzone = {
+      alto: dropzone.clientHeight,
+      ancho: dropzone.clientWidth
+    };
+    console.log();
+    var miniatura = "";
+    html2canvas(dropzone, {
+      onrendered(canvas) {
         miniatura = canvas.toDataURL();
       }
-   });
+    });
     //miniatura =  document.getElementById('miniatura').src;
-   
-   if(hash === "nueva-plantilla"){
-      tipo="plantilla";
-      //navigate(id);
-   }else if(hash === "nuevo-tablero"){
-      tipo="tablero";
+
+    if (hash === "nueva-plantilla") {
+      tipo = "plantilla";
       navigate(id);
-   }else if(hashValido.test(hash)){
-     id = hash;
-     let elemento = JSON.parse(localStorage.getItem(id));
-     tipo = elemento.tipo
-     console.log('id',id)
-   }
-   setTimeout(function() {
-     let datosAExportar = {
-       id: id,
-       contenido: dropzoneOuter,
-       tamano: tamanoDropzone,
-       titulo: "Titulo sin determinar",
-       tipo: tipo,
-       miniatura: miniatura
-     } 
-     var datos = JSON.stringify(datosAExportar);
-     localStorage.setItem(id, datos);
-   }, 10);
-   
-   console.log('guardado');
-});
+    } else if (hash === "nuevo-tablero") {
+      tipo = "tablero";
+      navigate(id);
+    } else if (hashValido.test(hash)) {
+      id = hash;
+      let elemento = JSON.parse(localStorage.getItem(id));
+      tipo = elemento.tipo;
+      console.log("id", id);
+    }
+    setTimeout(function() {
+      let datosAExportar = {
+        id: id,
+        contenido: dropzoneOuter,
+        tamano: tamanoDropzone,
+        titulo: "Titulo sin determinar",
+        tipo: tipo,
+        miniatura: miniatura
+      };
+      var datos = JSON.stringify(datosAExportar);
+      localStorage.setItem(id, datos);
+    }, 10);
+
+    console.log("guardado");
+  });
 
 /*
 
