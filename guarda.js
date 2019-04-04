87510766f9ad70a3f7326faa47b6b7095230d22d
Carmen Lopez <carmenlop94@gmail.com>
2019-04-04 11:20:22 +0000

    add miniatura

--- guarda.js
+++ guarda.js
@@ -14,28 +14,48 @@ document.getElementById("botonDescarga").addEventListener('click', function(even
        alto: dropzone.clientHeight,
        ancho: dropzone.clientWidth
    }
+   console.log()
+   var miniatura ="";
+   html2canvas(dropzone, {
+      onrendered (canvas) {
+        miniatura = canvas.toDataURL();
+      }
+   });
+    //miniatura =  document.getElementById('miniatura').src;
+   
    if(hash === "nueva-plantilla"){
       tipo="plantilla";
-      navigate(id);
+      //navigate(id);
    }else if(hash === "nuevo-tablero"){
       tipo="tablero";
       navigate(id);
    }else if(hashValido.test(hash)){
      id = hash;
+     let elemento = JSON.parse(localStorage.getItem(id));
+     tipo = elemento.tipo
      console.log('id',id)
    }
-   let datosAExportar = {
+   setTimeout(function() {
+     let datosAExportar = {
        id: id,
        contenido: dropzoneOuter,
        tamano: tamanoDropzone,
        titulo: "Titulo sin determinar",
-       tipo: tipo
-   } 
-   var datos = JSON.stringify(datosAExportar);
-   localStorage.setItem(id, datos);
+       tipo: tipo,
+       miniatura: miniatura
+     } 
+     var datos = JSON.stringify(datosAExportar);
+     localStorage.setItem(id, datos);
+   }, 10);
+   
    console.log('guardado');
 });
 
+/*
 
-
-
+function guardamuchos(){
+  for(let i=0;i<500;i++){
+  document.getElementById("botonDescarga").click;
+}
+}
+*/
