d0a1ee4f7d62d8116fc8833057a63680c1b02224
calope03 <calope03@ucm.es>
2019-04-10 18:24:53 +0200

    cambio buscador para que funcione tactil

--- busqueda.js
+++ busqueda.js
@@ -1,23 +1,44 @@
 const inputBusquedaSlctr = document.getElementById('inputBusqueda');
 
+const resultadosSlctr = document.getElementById('resultadosBusqueda');
+
+const btnBusquedaSlctr = document.getElementById('btnBusqueda');
+
 inputBusquedaSlctr.addEventListener('keyup', buscar);
+
+btnBusquedaSlctr.addEventListener('click', buscarOcerrar);
+
+function buscarOcerrar(event){
+  console.log(event);
+  if(btnBusquedaSlctr.classList.contains('search')){
+    let textoBusqueda = inputBusquedaSlctr.value;
+    ajaxHandler('https://api.arasaac.org/api/pictograms/es/search/'+textoBusqueda, listarResultados);
+  }else if(btnBusquedaSlctr.classList.contains('close')){
+    resultadosSlctr.style.display = 'none';
+    btnBusquedaSlctr.classList.remove('close');
+    btnBusquedaSlctr.classList.add('search');
+    btnBusquedaSlctr.innerHTML = `<span class="input-group-text"><i class="fas fa-search"></i></span>`;
+  }
+}
 
 function buscar(event){
     if(event.keyCode === 13){
         let textoBusqueda = inputBusquedaSlctr.value;
-        ajaxHandler('https://api.arasaac.org/api/pictograms/es/search/'+textoBusqueda, listarResultados)
-        
+        ajaxHandler('https://api.arasaac.org/api/pictograms/es/search/'+textoBusqueda, listarResultados);
     }
 }
 
 function listarResultados(data){
-    let resultadosSlctr = document.getElementById('resultadosBusqueda');
+    
     resultadosSlctr.style.display = 'flex';
     let resultadosHTML = "";
     data.forEach(function(element){
         resultadosHTML += renderPicto(element);
     });
     resultadosSlctr.innerHTML = resultadosHTML;
+    btnBusquedaSlctr.classList.remove('search');
+    btnBusquedaSlctr.classList.add('close');
+    btnBusquedaSlctr.innerHTML = `<span class="input-group-text"><i class="fas fa-times"></i></span>`;
     recalculaAlturaPictogramas();
     //console.log(resultadosHTML);
 }
